695c8bd4b04103482373dd13c2262cc0d1059674
laderast <roshioshi@gmail.com>
2017-12-19 23:26:28 -0800

    adding more tests

--- tests/testInfer.R
+++ tests/testInfer.R
@@ -10,5 +10,14 @@ test_that("specify arguments", {
   expect_error(specify(mtcars, response=blah))
   expect_equal(ncol(specify(mtcars, formula = mpg ~ wt)), 2)
   expect_error(specify(mtcars, formula = mpg ~ blah))
+  expect_equal(class(specify(mtcars, formula = mpg ~ wt))[1], "infer")
+  
+  
+})
+
+source("R/generate.R")
+
+test_that("generate arguments", {
+  
   
 })
